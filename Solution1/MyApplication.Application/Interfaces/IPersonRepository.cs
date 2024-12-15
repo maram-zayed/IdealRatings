7d5b04977208d54efea390b8c27b1a3e27033519
maram <maram@DESKTOP-BGB3S1E>
2024-12-15 21:17:29 +0200

    .

--- Solution1/MyApplication.Application/Interfaces/IPersonRepository.cs
+++ Solution1/MyApplication.Application/Interfaces/IPersonRepository.cs
@@ -2,7 +2,5 @@
 
 public interface IPersonRepository
 {
-    //Task<IEnumerable<Domain.Entities.Person>> GetAllAsync();
-    //Task<IEnumerable<Domain.Entities.Person>> GetFilteredAsync(string nameFilter);
     Task<IEnumerable<Domain.Entities.Person>> GetAllAsync(string? filterByName = null);
 }
